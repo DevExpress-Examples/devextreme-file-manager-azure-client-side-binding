ba159cfc3ce685c4dc2df06447a95b6d9390a407
Tom <artem.kurchenko@devexpress.com>
2023-12-15 15:07:55 +0400

    Add React

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,18 +1,81 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
 
-function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
+import FileManager, { Permissions } from 'devextreme-react/file-manager';
+
+import { getAzureFileSystemProvider } from './api/azure.custom.provider';
+
+const allowedFileExtensions: string[] = [];
+
+export default function App(): JSX.Element {
+  const [requests, setRequests] = useState<
+  { method: string; urlPath: string; queryString: string }[]
+  >([]);
+  const onRequestExecuted = useCallback(
+    ({
+      method,
+      urlPath,
+      queryString,
+    }: {
+      method: string;
+      urlPath: string;
+      queryString: string;
+    }): void => {
+      const request = { method, urlPath, queryString };
+      setRequests((requests) => [request, ...requests]);
+    },
+    [],
+  );
+  const fileSystemProvider = useMemo(() => getAzureFileSystemProvider(
+    'https://localhost:7049/api/file-manager-azure-access',
+    onRequestExecuted,
+  ), []);
+
   return (
     <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+      <FileManager
+        id="file-manager"
+        fileSystemProvider={fileSystemProvider}
+        allowedFileExtensions={allowedFileExtensions}
+      >
+        <Permissions download={true}></Permissions>
+        {/* uncomment the code below to enable file/directory management */}
+        {/* <Permissions
+              create={true}
+              copy={true}
+              move={true}
+              delete={true}
+              rename={true}
+              upload={true}
+              download={true}>
+            </Permissions> */}
+      </FileManager>
+      <div id="request-panel">
+        {requests.map((r, i) => (
+          <div key={i} className="request-info">
+            <div className="parameter-info">
+              <div className="parameter-name">Method:</div>
+              <div className="parameter-value dx-theme-accent-as-text-color">
+                {r.method}
+              </div>
+            </div>
+            <div className="parameter-info">
+              <div className="parameter-name">Url path:</div>
+              <div className="parameter-value dx-theme-accent-as-text-color">
+                {r.urlPath}
+              </div>
+            </div>
+            <div className="parameter-info">
+              <div className="parameter-name">Query string:</div>
+              <div className="parameter-value dx-theme-accent-as-text-color">
+                {r.queryString}
+              </div>
+            </div>
+            <br />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
